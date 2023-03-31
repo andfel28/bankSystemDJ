e3e82f18153174b26571809badc78d6c6e3af64d
afgiraldoveri <afgiraldo@utp.edu.co>
2023-03-31 14:16:06 -0500

    1 commit

--- bankSystemDJ/settings.py
+++ bankSystemDJ/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -119,11 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 # test
 
-STATIC_URL =        'static/'
+STATIC_URL =        '/static/'
 """STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)"""
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'),]
 #STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-#STATIC_ROOT = BASE_DIR + "/static"
+STATIC_ROOT = BASE_DIR + "/staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
